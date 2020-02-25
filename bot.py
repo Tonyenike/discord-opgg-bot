e50bc1430a2d2b6ce6bfafe5e9e02b48b93a026e
Tonyenike <tonyenike@gmail.com>
2020-02-25 15:00:18 -0800

    Added OPGG rank, champion, reformatted table

--- bot.py
+++ bot.py
@@ -1,15 +1,26 @@
 import discord
 import messages
 import requests
+from tabulate import tabulate
 import os, sys, time
 from bs4 import BeautifulSoup
+import math
+import numpy
 
 discord_api_key="NjcwNDY1NDA4MzQ3Nzk5NTgy.XjH-4A.5ihcbTXn4-KBzZjBUMuhGAGc6No"
 DEVELOPMENT_MODE = True
 server_list = ["na", "euw", "eune", "ru", "kr", "lan", "oce", "br", "las", "tr", "cn", "jp"]
-version = "1.3.0"
+version = "1.4.0"
+author  = "Me Too Thanks#7924"
 
-async def process_commands(text_words, message):
+ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(math.floor(n/10)%10!=1)*(n%10<4)*n%10::4])
+
+def argsort(myArray):
+    array = numpy.array(myArray)
+    order = array.argsort()
+    return order.argsort()
+
+async def process_commands(selfie, text_words, message):
     resp = {}
     resp['server_name'] = "na"
     for i in range(len(text_words)):
@@ -24,9 +35,9 @@ async def process_commands(text_words, message):
             text_words.pop(i + 1)
             text_words.pop(i)
         elif text_words[i] == '!shutdown':
-            if(str(message.author) == "Me Too Thanks#7924"):
+            if(str(message.author) == author):
                 await message.channel.send(messages.goodbye)
-                await self.close()
+                await selfie.close()
             else:
                 await message.channel.send(messages.no)
             return None
@@ -47,6 +58,7 @@ class MyClient(discord.Client):
         global DEVELOPMENT_MODE
         if len(sys.argv) == 1:
             DEVELOPMENT_MODE = True
+            print("DEVELOPMENT MODE IS ENABLED")
         elif sys.argv[1] == "prod":
             DEVELOPMENT_MODE = False
         super().__init__()
@@ -59,12 +71,12 @@ class MyClient(discord.Client):
             return
         text_words = message.content.split()
         num_words = len(text_words)
-        if '!opgg' ==  text_words[0]:
+        if len(text_words) and '!opgg' ==  text_words[0]:
             if num_words == 1:
                 await message.channel.send(messages.greeting)
                 return
             text_words.pop(0)
-            respy = await process_commands(text_words, message)
+            respy = await process_commands(self, text_words, message)
             if respy is None:
                 return
             server_name = respy['server_name']
@@ -95,7 +107,7 @@ class MyClient(discord.Client):
             if len(game_history) == 0:
                 await message.channel.send("`" + user_name_on_opgg + "` has not played any games recently")
                 return
-            gameModes, victory_or_defeat, kdas, kp, score = [], [], [], [], []
+            gameModes, victory_or_defeat, kdas, kp, score, champion, rank = [], [], [], [], [], [], []
             match_detail_url_1 = "https://" + server_name + ".op.gg/summoner/matches/ajax/detail/gameId="
             match_detail_url_2 = "&summonerId=" + str(summoner_id) + "&gameTime="
             for el in game_history:
@@ -105,24 +117,34 @@ class MyClient(discord.Client):
                 match_detail_resp = requests.get(match_detail_url)
                 match_detail_soup = BeautifulSoup(match_detail_resp.text, "html.parser")
                 game_mode_str = el.find(class_="GameType").string.split()[0]
-                game_mode_str = "Solo" if (game_mode_str == "Ranked") else game_mode_str[0:4]
-                gameModes.append        (game_mode_str + " "*(4 - len(game_mode_str)))
-                victory_or_defeat.append("|Loss  " if (el.find(class_="Win") is None) else "|Win   ")
-                kdas.append             ("|" + el.find("span", class_="KDARatio").string.split()[0][0:4])
-                kp.append               ("|" + el.find("div", class_="CKRate").string.split()[1])
+                game_mode_str = "Solo" if (game_mode_str == "Ranked") else game_mode_str
+                gameModes.append(game_mode_str)
+                champion.append(el.find(class_="ChampionName").find('a').get_text())
+                victory_or_defeat.append("Loss" if (el.find(class_="Win") is None) else "Win")
+                kdas.append             (el.find("span", class_="KDARatio").string.split()[0][0:4])
+                kp.append               (el.find("div", class_="CKRate").string.split()[1])
                 sum_names  = match_detail_soup.findAll("td", class_="SummonerName Cell")
                 score_items = match_detail_soup.findAll("div", class_="OPScore Text")
-                for i in range(10):
-                    if sum_names[i].find('a').get_text() == user_name_on_opgg:
-                        if len(score_items) is 0:
-                            score.append("|N/A")
-                        else:
-                            score.append            ("|" + score_items[i].get_text())
-            res = [i + j + k + l + m for i, j, k, l, m in zip(gameModes, victory_or_defeat, kdas, kp, score)]
-            myString = '\n'.join(res)
-            myDivider="\n" + "-" * 26 + "\n"
+                if len(score_items) is 0:
+                    score.append("N/A")
+                    rank.append("N/A")
+                else:
+                    other_scores=[]
+                    for i in range(10):
+                        other_scores.append(float(score_items[i].get_text()))
+                        if sum_names[i].find('a').get_text() == user_name_on_opgg:
+                            myScoreIndex = i
+                            score.append(score_items[i].get_text())
+                    ranks = argsort(other_scores)
+                    rank.append(str(ordinal(10 - ranks[myScoreIndex]))) 
+            res = [[i, j, k, l, m, n, o] for i, j, k, l, m, n, o in zip(gameModes, victory_or_defeat, champion, kdas, kp, score, rank)]
+            headers = ["Game", "Result", "Champion", "KDA", "KP", "OPGG", "Rank"]
+            res.insert(0, headers)
             development_str = "DEVELOPMENT MODE:\n" if DEVELOPMENT_MODE else ""
-            await message.channel.send('```\n' + development_str + 'STATS FOR ' + user_name_on_opgg + ": " + myDivider + "Game|Result|KDA |KP |OPGG" + myDivider +  myString + "```")
+            table_str = tabulate(res, headers="firstrow", tablefmt="fancy_grid", numalign="left")
+            final_string =  '```\n' + development_str + 'STATS FOR ' + user_name_on_opgg + ":\n" + table_str + "```" 
+            await message.channel.send(final_string)
+           
             return
 
 def main():
